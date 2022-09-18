59b6a36d0a8f1e35cc6c993f126bd6dd0529c026
ulejoyeu <krilius974@gmail.com>
2022-09-18 17:27:04 +0200

    lazer juice

--- SimpleLazer.gd
+++ SimpleLazer.gd
@@ -26,6 +26,7 @@ func _on_collision(area):
 			if area_parent.type_ in ["friend", "enemy"]:
 				SPEED = 0
 				get_node("./Area2D/AnimatedSprite").play("explode")
+				handle_tween(area_parent)
 				time_till_flush = 0.25
 			elif area_parent.type_ == "bonus":
 				queue_free()
@@ -35,4 +36,31 @@ func _on_collision(area):
 			if area_parent.type_ in ["friend", "enemy", "bonus"]:
 				SPEED = 0
 				get_node("./Area2D/AnimatedSprite").play("explode")
+				handle_tween(area_parent)
 				time_till_flush = 0.25
+
+
+func handle_tween(area_parent):
+	if area_parent.type_ == "enemy":
+		show_tween_label("+ %d" % area_parent.points, Color(0, 1, 0))
+	if area_parent.type_ == "friend":
+		show_tween_label("- %d" % area_parent.points, Color(1, 0, 0))
+
+
+func show_tween_label(text, color):
+	var tween = $Tween
+	$TweenLabel.text = text
+	$TweenLabel.set("custom_colors/font_color", color)
+	$TweenLabel.visible = true
+	tween.stop_all()
+	tween.interpolate_property(
+		$TweenLabel,
+		"rect_position:y",
+		$TweenLabel.rect_position.y,
+		$TweenLabel.rect_position.y - 64,
+		0.25,
+		Tween.TRANS_LINEAR,
+		Tween.EASE_IN_OUT
+	)
+	tween.start()
+
